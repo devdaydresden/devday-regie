d39c1677010877369da4836c3fbdd727d41564ca
Stefan Bethke <stb@lassitu.de>
2021-04-07 17:37:25 +0200

    Refactor, improve sound
    Make it possible to share a multiview over Jitsi by creating a loopback 
    device for piping the OBS monitor sound to a virtual microphone.

--- roles/devday-regie/files/startx.sh
+++ roles/devday-regie/files/startx.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 /usr/bin/Xvfb :0 -screen 0 1680x1050x24 -dpi 72 &
+
 export DISPLAY=:0
 (
 	sleep 30;
 	~/configure-session.sh
 ) &
 /usr/bin/pulseaudio --start
+# /usr/bin/x11vnc -usepw -shared -loop &
 /usr/bin/mate-session
